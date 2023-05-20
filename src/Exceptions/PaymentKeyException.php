c7f0362f366920566aedc162b4acb338e572d185
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-05-20 19:55:18 +0300

    added exceptions for handling paymob api errors

---
+++ src/Exceptions/PaymentKeyException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Skrskr\Paymob\Exceptions;
+
+use Exception;
+
+class PaymentKeyException extends Exception
+{
+
+}
---
+++ src/Exceptions/RegisterOrderException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Skrskr\Paymob\Exceptions;
+
+use Exception;
+
+class RegisterOrderException extends Exception
+{
+
+}
