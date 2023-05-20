91596dc64dba44f8dfdbf5356aaf0fc9d7c1bec5
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2023-05-20 23:04:48 +0300

    added after Transaction failed and successed events

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -7,6 +7,8 @@ namespace Skrskr\Paymob\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Skrskr\Paymob\Events\TransactionFailedEvent;
+use Skrskr\Paymob\Events\TransactionSuccessedEvent;
 use Skrskr\Paymob\Http\Middleware\VerifyWebhookHmac;
 
 class WebhookController extends Controller
@@ -28,14 +30,18 @@ class WebhookController extends Controller
         \Log::info("Transaction Process Request : " . json_encode($request->toArray()));
         $type = $request->get('type');
         $obj = $request->get('obj');
+        $payload = json_decode($request->getContent(), true);
 
-        $orderId = $obj['order']['merchant_order_id'];
-
+        
         if (($type == "TRANSACTION")) {
+            // $orderId = $obj['order']['merchant_order_id'];
+
             if (($obj['success'] == true) && ($obj['pending'] == false) && ($obj['is_refunded'] == false)) {
                 \Log::info("Transaction Process Success");
+                TransactionSuccessedEvent::dispatch($payload);
             } else if (($obj['success'] == false) && ($obj['pending'] == false) && ($obj['is_refunded'] == false)) {
                 \Log::info('Transaction failed');
+                TransactionFailedEvent::dispatch($payload);
             }
         }
     }
